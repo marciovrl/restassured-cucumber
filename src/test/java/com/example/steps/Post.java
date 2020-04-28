cbae1bb434dd26547b43cac2a96ee091c351b456
marcio lemos <contato.marciovrl@gmail.com>
2020-04-28 10:33:00 -0300

    implemented the tests

--- src/test/java/com/example/steps/Post.java
+++ src/test/java/com/example/steps/Post.java
@@ -1,25 +1,32 @@
 package com.example.steps;
 
 import cucumber.api.java.en.*;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
 
-import com.example.support.BaseAPI;
+import static io.restassured.RestAssured.*;
 
-public class Post extends BaseAPI {
+public class Post {
+
+    private RequestSpecification request;
+    private Response response;
+    private ValidatableResponse json;
+    private String ENDPOINT = "https://jsonplaceholder.typicode.com";
+
     @Given("^use endpoint posts$")
-    public void use_endpoint_posts() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void use_endpoint_posts() {
+        request = given().basePath("posts").contentType(ContentType.JSON);
     }
 
     @When("^ready all Post item$")
-    public void ready_all_Post_item() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void ready_all_Post_item() {
+        response = request.when().get(ENDPOINT);
     }
 
     @Then("^I view all Post items$")
-    public void i_view_all_Post_items() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    public void i_view_all_Post_items() {
+        json = response.then().statusCode(200);
     }
 }
