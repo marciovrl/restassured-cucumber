cd42bf00c02d76dca8ef65aa436b515a03f52a7d
marcio lemos <contato.marciovrl@gmail.com>
2020-11-17 10:20:35 -0300

    implemente new scenarios

--- src/test/features/Post.feature
+++ src/test/features/Post.feature
@@ -1,10 +1,18 @@
 #language:en 
 
-Feature: List item Post
+Feature: Post item
 
     Background: Use endpoint posts
         Given use endpoint posts
 
-    Scenario: List all itens Post
+    Scenario: List all Post itens
         When ready all Post item
         Then I view all Post items
+
+    Scenario: Add new Post item
+        When add new Post item
+        Then I view the new Item
+
+    Scenario: Delete Post item
+        When delete new Post item
+        Then Item was delete
--- src/test/java/steps/Post.java
+++ src/test/java/steps/Post.java
@@ -1,6 +1,5 @@
 package steps;
 
-import cucumber.api.java.Before;
 import cucumber.api.java.en.*;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -9,28 +8,56 @@ import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.*;
+
 public class Post {
 
     private RequestSpecification request;
     private Response response;
 
-    @Before
-    public void setUp() {
-        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
-    }
-
     @Given("use endpoint posts")
     public void use_endpoint_posts() {
-        request = given().contentType(ContentType.JSON);
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+        RestAssured.basePath = "posts";
     }
 
     @When("ready all Post item")
     public void ready_all_Post_item() {
-        response = request.when().get("/posts");
+        request = given().contentType(ContentType.JSON);
+        response = request.when().get();
     }
 
     @Then("I view all Post items")
     public void i_view_all_Post_items() {
+        response.then().statusCode(200);
+    }
+
+    @When("add new Post item")
+    public void add_new_Post_item() {
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("title", "Testing");
+        params.put("body", "Testing API example");
+
+        request = given().contentType(ContentType.JSON).body(params);
+        response = request.when().post();
+    }
+
+    @Then("^I view the new Item$")
+    public void i_view_the_new_Item() {
+        response.then().statusCode(201).body("title", is("Testing"));
+    }
+
+    @When("delete new Post item")
+    public void delete_new_Post_item() {
+        request = given().contentType(ContentType.JSON);
+        response = request.when().delete("/1");
+    }
+
+    @Then("Item was delete")
+    public void item_was_delete() {
         response.then().statusCode(200);
     }
 }
